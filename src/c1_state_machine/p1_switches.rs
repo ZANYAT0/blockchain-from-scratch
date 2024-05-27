896529c236135406e13d401406db124c7ad310ea
Serhii Proskurin <zanyato094@gmail.com>
2024-05-27 06:41:34 +0200

    switches

--- src/c1_state_machine/p1_switches.rs
+++ src/c1_state_machine/p1_switches.rs
@@ -2,6 +2,7 @@
 //! In these examples, we use actually switch boards as the state machine. The state is,
 //! well, just the state of the switches.
 
+use crate::c1_state_machine::p6_open_ended::State;
 use super::StateMachine;
 
 /// This state machine models a single light switch.
@@ -15,7 +16,7 @@ impl StateMachine for LightSwitch {
     type Transition = ();
 
     fn next_state(starting_state: &bool, t: &()) -> bool {
-        todo!("Exercise 1")
+        !starting_state
     }
 }
 
@@ -42,7 +43,14 @@ impl StateMachine for WeirdSwitchMachine {
     type Transition = Toggle;
 
     fn next_state(starting_state: &TwoSwitches, t: &Toggle) -> TwoSwitches {
-        todo!("Exercise 2")
+        return match t {
+            Toggle::FirstSwitch => {
+                TwoSwitches { first_switch: !starting_state.first_switch, second_switch: false }
+            }
+            Toggle::SecondSwitch => {
+                TwoSwitches { first_switch: starting_state.first_switch, second_switch: !starting_state.second_switch }
+            }
+        }
     }
 }
 
